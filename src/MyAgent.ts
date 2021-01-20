7000f08cc76231a676e52350255d120476fbfa47
Devin Liu <Devin.Liu@bentley.com>
2021-01-20 13:01:45 +0800

    初始化工程

--- src/MyAgent.ts
+++ src/MyAgent.ts
@@ -7,6 +7,9 @@ import {
   EventSubscription,
   IModelHubClient,
   IModelHubEventType,
+  HubIModel,
+  IModelQuery,
+  IModelBankClient,
 } from "@bentley/imodelhub-client";
 import {
   ApplicationType,
@@ -18,10 +21,30 @@ import {
 } from "@bentley/imodeljs-backend";
 import { IModelVersion, SyncMode } from "@bentley/imodeljs-common";
 import { AgentConfig } from "./AgentConfig";
-
+import { ContextRegistryClient } from "@bentley/context-registry-client";
+import { Guid } from "@bentley/bentleyjs-core";
+import {
+  AuthorizedClientRequestContext,
+  AccessToken,
+  UserInfo,
+} from "@bentley/itwin-client";
+import {
+  BasicAccessToken,
+  IModelBankBasicAuthorizationClient,
+} from "@bentley/imodelhub-client/lib/imodelbank/IModelBankBasicAuthorizationClient";
+const email = "test";
+const password = "test";
+export function createRequestContext() {
+  return new AuthorizedClientRequestContext(
+    BasicAccessToken.fromCredentials({
+      email,
+      password,
+    })
+  );
+}
 export class MyAgent {
   private readonly config: AgentConfig;
-  private readonly hubClient: IModelHubClient;
+  private readonly hubClient: IModelBankClient;
   private readonly oidcClient: AgentAuthorizationClient;
 
   private hubSubscription?: EventSubscription;
@@ -29,13 +52,18 @@ export class MyAgent {
 
   constructor(config: AgentConfig) {
     this.config = config;
-    this.hubClient = new IModelHubClient(new AzureFileHandler());
+    const url = "http://localhost:4000";
+    const imodelClient = new IModelBankClient(url, undefined);
+    this.hubClient = imodelClient;
     this.oidcClient = new AgentAuthorizationClient({
-      clientId: config.CLIENT_ID!,
-      clientSecret: config.CLIENT_SECRET!,
+      clientId: config.CLIENT_ID,
+      clientSecret: config.CLIENT_SECRET,
       scope: "imodelhub context-registry-service:read-only urlps-third-party",
     });
   }
+  // •	imodelhub
+  // •	urlps-third-party
+  // •	context-registry-service:read-only
 
   public async initialize() {
     const hostConfig = new IModelHostConfiguration();
@@ -51,7 +79,7 @@ export class MyAgent {
     const eventTypes = [IModelHubEventType.ChangeSetPostPushEvent];
     this.hubSubscription = await this.hubClient.events.subscriptions.create(
       ctx,
-      this.config.IMODEL_ID!,
+      this.config.IMODEL_ID,
       eventTypes
     );
     console.log(
@@ -77,34 +105,88 @@ export class MyAgent {
       ctx,
       authCallback,
       this.hubSubscription.wsgId,
-      this.config.IMODEL_ID!,
+      this.config.IMODEL_ID,
       listener
     );
   }
 
   public async run(version = IModelVersion.latest()) {
-    const ctx = await this.createContext();
-
+    console.log(version);
+    const contextId = "9374a302-8743-403e-ad03-6c49ef13c15e";
+    const imjs_test_context_id = "acd4f071-02d8-4c62-8af3-6b2c77b19a5c";
+    const imjs_test_imodel_id = "8f9f2aa4-0711-4a07-9574-7f6bb2b3b755";
+    const req = await createRequestContext();
+    const hg = await BriefcaseManager.imodelClient.iModels.get(req, contextId);
+    console.log(hg);
+    // const briefcaseProps = await BriefcaseManager.download(
+    //   ctx,
+    //   this.config.CONTEXT_ID,
+    //   this.config.IMODEL_ID,
+    //   downloadOptions,
+    //   version
+    // );
+    // const ctx = await this.createContext();
+    // console.log(version);
+    // console.log("开始了");
+    // const path = "D:/MS/nba.bim";
+    // const iModelName = "reinstateChangeTest";
+    // const projectName = "StudyImodel111";
+    // const projectId = "23383876-c4a1-4c6b-9dc4-f3f2cf828606";
+    // const client = new ContextRegistryClient();
+    // const uf = ctx.accessToken.getUserInfo();
+    // console.log(uf);
+    // const downloadOptions = { syncMode: SyncMode.PullOnly };
+    // ctx.enter();
+    // const briefcaseProps = await BriefcaseManager.download(
+    //   ctx,
+    //   this.config.CONTEXT_ID,
+    //   this.config.IMODEL_ID,
+    //   downloadOptions,
+    //   version
+    // );
+    // console.log(briefcaseProps);
+    // const p = await client.getProject(ctx, {
+    //   $select: "*",
+    //   $filter: `Name+eq+'${projectName}'`,
+    // });
+    // console.log(p.name);
+    // const tk = new AccessToken();
+    // const u = new UserInfo("devin.liu@bentley.com");
+    // tk.setUserInfo(u);
+    // const token = new AuthorizedClientRequestContext(tk);
+    // const p = "D:/ORD";
+    // BriefcaseManager.initialize(p);
+    // const hu = await BriefcaseManager.create(
+    //   ctx,
+    //   Guid.createValue(),
+    //   "NBA2020.bim",
+    //   { rootSubject: { name: "TestSubject" } }
+    // );
+    // console.log(hu);
+    // console.log(rw.name);
+    // const projectId = Guid.createValue();
+    // const rwIModelId = await BriefcaseManager.create(
+    //   ctx,
+    //   projectId,
+    //   iModelName,
+    //   { rootSubject: { name: "TestSubject" } }
+    // );
+    // console.log(rwIModelId);
+    // console.log(project);
+    // const hu = await BriefcaseManager.imodelClient.iModel.get(
+    //   ctx,
+    //   this.config.IMODEL_ID
+    // );
+    // console.log(hu.name);
     // Download iModel
-    const downloadOptions = { syncMode: SyncMode.FixedVersion };
-    const briefcaseProps = await BriefcaseManager.download(
-      ctx,
-      this.config.CONTEXT_ID!,
-      this.config.IMODEL_ID!,
-      downloadOptions,
-      version
-    );
-    ctx.enter();
-
-    // Open iModel
-    const iModel = await BriefcaseDb.open(ctx, briefcaseProps.key);
-    ctx.enter();
-
-    // TODO....
-    console.log(iModel.rootSubject.name);
-
-    // Close iModel
-    iModel.close();
+    // ctx.enter();
+    // // Open iModel
+    // const iModel = await BriefcaseDb.open(ctx, briefcaseProps.key);
+    // ctx.enter();
+    // // TODO....
+    // console.log(iModel.rootSubject.name);
+    // // Close iModel
+    // iModel.close();
   }
 
   private async createContext() {
@@ -123,7 +205,7 @@ export class MyAgent {
     if (this.hubSubscription) {
       await this.hubClient.events.subscriptions.delete(
         await this.createContext(),
-        this.config.IMODEL_ID!,
+        this.config.IMODEL_ID,
         this.hubSubscription.wsgId
       );
       console.log(
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { LogLevel, Logger } from "@bentley/bentleyjs-core";
+import { Logger, LogLevel } from "@bentley/bentleyjs-core";
 import { IModelVersion } from "@bentley/imodeljs-common";
-import { MyAgent } from "./MyAgent";
-import { loadAgentConfig } from "./AgentConfig";
 import * as minimist from "minimist";
 import { onAnyKey } from "onAnyKey";
-import { AuthorizedBackendRequestContext } from "@bentley/imodeljs-backend";
+import { loadAgentConfig } from "./AgentConfig";
+import { MyAgent } from "./MyAgent";
+import { BriefcaseManager } from "@bentley/imodeljs-backend";
 
 const argv = minimist(process.argv.slice(2));
 
@@ -14,31 +14,33 @@ Logger.setLevelDefault(LogLevel.Error);
 console.log("MY AGENT STARTED");
 
 (async () => {
-  // try {
-  const config = loadAgentConfig();
-  const agent = new MyAgent(config);
-  console.log(config.CLIENT_ID);
-  console.log(config.CLIENT_SECRET);
-  await agent.initialize();
-  await agent.run();
-  //   if (argv.latest) {
-  //     await agent.run();
-  //   } else if (argv.changeset) {
-  //     await agent.run(IModelVersion.asOfChangeSet(argv.changeset));
-  //   } else {
-  //     await agent.listen();
-  //     console.log(
-  //       "MY AGENT NOW LISTENING FOR HUB EVENTS -- press any key to stop"
-  //     );
-  //     await onAnyKey();
-  //   }
-
-  //   await agent.terminate();
-  //   console.log("MY AGENT FINISHED");
-  // } catch (error) {
-  //   console.error(error, "Unhandled exception thrown in my-agent");
-  //   process.exitCode = 1;
-  // }
+  try {
+    const config = loadAgentConfig();
+    const agent = new MyAgent(config);
+    await agent.initialize();
+    console.log("初始化完毕");
+    if (true) {
+      await agent.run();
+    } else if (argv.changeset) {
+      await agent.run(IModelVersion.asOfChangeSet(argv.changeset));
+    } else {
+      // await agent.listen();
+      console.log(
+        "MY AGENT NOW LISTENING FOR HUB EVENTS -- press any key to stop"
+      );
+      await onAnyKey();
+    }
+    // await agent.terminate();
+    // console.log("MY AGENT FINISHED");
+    // BriefcaseManager.initialize("D:/imodel-exampleagent-starter");
+    // const r = BriefcaseManager.imodelClient.fileHandler!.basename(
+    //   "D:/imodel-exampleagent-starter/tslint.json"
+    // );
+    // console.log(r);
+  } catch (error) {
+    console.error(error, "Unhandled exception thrown in my-agent");
+    process.exitCode = 1;
+  }
 })();
 
 process.on("unhandledRejection", (_reason, promise) => {
